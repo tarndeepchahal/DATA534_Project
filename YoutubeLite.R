7d46adad0454af5999d5968981757edbd8b5a62f
unknown <vincentphan3004@gmail.com>
2020-01-30 14:18:50 -0800

    upda R package

--- YoutubeLite.R
+++ YoutubeLite.R
@@ -5,22 +5,22 @@ video_id <- '9S-U5f140-U'
 
 get_views <- function (key, id) {
   response <- GET('https://www.googleapis.com/youtube/v3/videos', query = list(part = 'statistics', id = id, key = key))
-
+  
   response <- content(response)
   clean_res <- response$items[[1]]
   stats <- clean_res$statistics
-
+  
   list(id = clean_res$id,
        viewCount = stats$viewCount)
 }
 
 get_likes <- function (key, id) {
   response <- GET('https://www.googleapis.com/youtube/v3/videos', query = list(part = 'statistics', id = id, key = key))
-
+  
   response <- content(response)
   clean_res <- response$items[[1]]
   stats <- clean_res$statistics
-
+  
   list(id = clean_res$id,
        likeCount = stats$likeCount,
        dislikeCount = stats$dislikeCount)
@@ -28,18 +28,18 @@ get_likes <- function (key, id) {
 
 get_comments <- function (key, id) {
   response <- GET('https://www.googleapis.com/youtube/v3/videos', query = list(part = 'statistics', id = id, key = key))
-
+  
   response <- content(response)
   clean_res <- response$items[[1]]
   stats <- clean_res$statistics
-
+  
   list(id = clean_res$id,
        commentCount = stats$commentCount)
 }
 
 get_details <- function (key, id) {
   response <- GET('https://www.googleapis.com/youtube/v3/videos', query = list(part = 'snippet', id = id, key = key))
-
+  
   response <- content(response)
   list(videoId = response$items[[1]]$id,
     publishedAt = response$items[[1]]$snippet$publishedAt,
@@ -49,10 +49,3 @@ get_details <- function (key, id) {
     categoryId = response$items[[1]]$snippet$categoryId)
 }
 
-
-get_views(API_key, video_id)
-get_likes(API_key, video_id)
-get_comments(API_key, video_id)
-get_details(API_key, video_id)
-
-?response
