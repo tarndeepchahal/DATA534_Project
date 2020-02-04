685000067e2432a95e705cc20e5ffa44c9530182
unknown <vincentphan3004@gmail.com>
2020-02-03 18:40:16 -0800

    add

--- main - Copy.R
+++ main - Copy.R
@@ -1,9 +1,9 @@
-#install.packages("YoutubeLite_0.1.0.zip",repos=NULL)
+install.packages("YoutubeLite_0.1.2.zip",repos=NULL)
+rm(list = ls(all. names = TRUE))
 library(YoutubeLite)
 #### FOR TESTING ####
 set_key('AIzaSyAdy65AkdC-E8--0DYB5osklXQFQOKAFjI')
 video_id <- 'https://www.youtube.com/watch?v=-DtSrdh5dHU'
-
 get_views(video_id)
 get_likes(video_id)
 get_commentCount(video_id)
